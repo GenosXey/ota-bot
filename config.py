c59f6c10ae48edb71866ed9525e5d327a1ba24e5
Noob Dev <kingceylegend@gmail.com>
2025-04-12 20:20:43 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 
 import os
 
-API_ID = int(os.getenv("API_ID", "12345678"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "24817837"))
+API_HASH = os.getenv("API_HASH", "acd9f0cc6beb08ce59383cf250052686")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7216818057:AAFyHDPbYHFbjVCGBG2Rir7Ai0YMDfGDbiM")
 
 # MongoDB URI (ex: mongodb+srv://user:pass@cluster0.mongodb.net/myDB)
-MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_connection_uri")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://altof2:123Bonjoure@cluster0.s1suq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = "kingfobot"
